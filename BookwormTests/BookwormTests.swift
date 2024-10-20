cd23a84ff433415597932a29b036e625a9042e70
MJMayankMJ <226301131@gkv.ac.in>
2024-10-20 09:28:38 +0530

    Initial Commit

---
+++ BookwormTests/BookwormTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormTests.swift
+//  BookwormTests
+//
+//  Created by Mayank Jangid on 10/20/24.
+//
+
+import Testing
+@testable import Bookworm
+
+struct BookwormTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
